1f397c0b1fb8538ee0933550e1091a00a3e5a4fe
Tanuj <Tanuj@cmu.edu>
2023-09-17 14:36:47 -0700

    Organize content.js
    I like this sequence of tasks, seems like it is working very efficiently

--- Resources/content.js
+++ Resources/content.js
@@ -1,22 +1,18 @@
-// browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-//    console.log("Received response: ", response);
-// });
-
-// browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//    console.log("Received request: ", request);
-// });
+//browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//   console.log("Received response: ", response);
+//});
+//
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   console.log("Received request: ", request);
+//});
 
 /**
- * Function to check and toggle the autoplay button on a YouTube video page.
- * If the page URL does not include 'youtube.com/watch', the function will return true.
- * If the autoplay button exists and is turned on, the function will click it.
- * If the autoplay button exists but is already off, the function will return true.
- *
- * @returns {boolean} - Returns true if the autoplay was turned off or the page is not a video page.
- *                       Returns false if the autoplay button wasn't found or the autoplay status is undefined.
+ * Function to find an element within a NodeList by its inner text.
+ * @param {NodeList} menuItems - The list of elements to search within.
+ * @param {string} text - The inner text to match.
+ * @returns {Element|null} - The matching element if found, or null if not found.
  */
-
-function findMenuItemByText(menuItems, text) {
+const findElementByInnerText = (menuItems, text) => {
     for (let i = 0; i < menuItems.length; i++) {
         if (menuItems[i].innerText === text) {
             return menuItems[i];
@@ -25,20 +21,25 @@ function findMenuItemByText(menuItems, text) {
     return null;
 };
 
-function openQualityMenu() {
+/**
+ * Function to find and set the quality button.
+ * Currently chooses highest quality option.
+ * @returns {boolean} - Returns true if quality successfully set
+ */
+const setQuality = () => {
     console.log("Opening quality menu");
     const button = document.querySelector('.ytp-settings-button');
     if (!button) {
         console.log("Button element not found");
-        return;
+        return false;
     }
     button.click();
 
     const menuItems = document.querySelectorAll('.ytp-menuitem-label');
-    const qualityMenuItem = findMenuItemByText(menuItems, 'Quality');
+    const qualityMenuItem = findElementByInnerText(menuItems, 'Quality');
     if (!qualityMenuItem) {
         console.log("Quality menu item not found");
-        return;
+        return false;
     }
     qualityMenuItem.click();
 
@@ -47,67 +48,75 @@ function openQualityMenu() {
     const qualityOptions = document.querySelectorAll('.ytp-menuitem-label');
     if (qualityOptions.length === 0) {
         console.log("No quality options found");
-        return;
+        return false;
     }
 
-let bestQualityOption = 0;
-let bestQualityElement;
+    let bestQualityOption = 0;
+    let bestQualityElement;
 
-for (const option of qualityOptions) {
-    // itemText is the first 4 characters of the string
-    const itemText = option.innerText.substring(0, 4);
-    // console.log(itemText);
-    if (validQualityText.includes(itemText)) {
-        const itemValue = parseInt(itemText);
-        if (itemValue > bestQualityOption) {
-        bestQualityOption = itemValue;
-        bestQualityElement = option;
+    for (const option of qualityOptions) {
+        // itemText is the first 4 characters of the string
+        const itemText = option.innerText.substring(0, 4);
+        // console.log(itemText);
+        if (validQualityText.includes(itemText)) {
+            const itemValue = parseInt(itemText);
+            if (itemValue > bestQualityOption) {
+            bestQualityOption = itemValue;
+            bestQualityElement = option;
+            }
         }
     }
-}
 
     if (bestQualityElement) {
         bestQualityElement.click();
         console.log("Highest quality option selected");
+        return true;
     }
     else {
-        console.log("No valid quality options found, probably too fast");
+        console.log("No valid quality options found, possibly too fast");
+        return false;
     }
 };
 
-
-const checkAutoplayButton = () => {
+/**
+ * Function to find and disable the autoplay button on a YouTube video page.
+ * If the autoplay button exists and is turned on, the function will disable it and return true.
+ * If the autoplay button exists but is already off, the function will return true.
+ * @returns {boolean} - Returns true if autoplay successfully turned off
+ */
+const setAutoplay = () => {
     console.log("Checking for autoplay button...");
-
     let autoplayButton = document.querySelector('button.ytp-button[data-tooltip-target-id="ytp-autonav-toggle-button"]');
     if (!autoplayButton) return false;
-
-    let autoplayStatus = autoplayButton.getAttribute('aria-label');
-
+    const autoplayStatus = autoplayButton.getAttribute('aria-label');
     // If the autoplay button exists and is turned on, click it
     if (autoplayStatus === 'Autoplay is on') {
         autoplayButton.click();
         console.log("Turned autoplay off!");
         return true;
     }
-
     // If the autoplay button exists but is already off
     if (autoplayStatus === 'Autoplay is off') {
         console.log("Autoplay is already off!");
         return true;
     }
-
     console.log("autoplayButton loaded, but not on or off!");
     return false;
 };
 
+
+
 /**
- * Handle 'yt-navigate-finish' event by starting an interval to check for the autoplay button
- * The function attempts to find the autoplay button and turn it off if it is on.
- * The interval is cleared once the autoplay is turned off or the maximum number of attempts is reached.
+ * Handle 'yt-navigate-finish' event by starting an interval to check success condition
+ * The function attempts to set quality and turn off autoplay.
+ * The interval is cleared once this succeeds or the maximum number of attempts is reached.
  */
 const handleNavigateFinish = () => {
     // Check for autoplay button when YouTube navigation finishes
+    if (!window.location.href.includes('youtube.com/watch')) {
+        console.log("No need to run any scripts here :)");
+        return;
+    }
     let checkAttempts = 0;
 
     const checkInterval = setInterval(() => {
@@ -115,21 +124,18 @@ const handleNavigateFinish = () => {
 
         console.log(`Attempt ${checkAttempts} to find autoplay button...`);
 
-        const autoplayOff = checkAutoplayButton();
-
+        const setQualityResult = setQuality();
+        if (!setQualityResult) {
+            console.log("setQualityResult was false, returning");
+            return;
+        }
+        const setAutoplayResult = setAutoplay();
         // If the autoplay was turned off, or the maximum number of attempts was reached, clear the interval
-        if (autoplayOff || checkAttempts >= 10) {
+        if (setAutoplayResult || checkAttempts >= 10) {
             clearInterval(checkInterval);
-            openQualityMenu();
         }
     }, 500); // Check every 500ms
 };
 
-if (!window.location.href.includes('youtube.com/watch')) {
-    console.log("No need to run any scripts here :)");
-}
-
-else {
-    window.addEventListener('yt-navigate-finish', handleNavigateFinish);
-    console.log("Listening for yt-navigate-finish.");
-}
+window.addEventListener('yt-navigate-finish', handleNavigateFinish);
+console.log("Listening for yt-navigate-finish.");
